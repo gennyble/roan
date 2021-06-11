9c858c5a3b8578f69ba5857513c444f4ae8f2f2a
Brad Alfirevic <brad@genbyte.dev>
2021-06-11 03:24:04 -0500

    Now working on android! messy code...

--- glandroid/src/app.rs
+++ glandroid/src/app.rs
@@ -2,9 +2,9 @@ use glow::{Buffer, HasContext, Program, VertexArray};
 use glutin::{
 	dpi::PhysicalSize,
 	event::{Event, WindowEvent},
-	event_loop::{ControlFlow, EventLoop},
-	window::WindowBuilder,
-	ContextBuilder,
+	event_loop::{ControlFlow, EventLoop, EventLoopWindowTarget},
+	window::{Window, WindowBuilder},
+	ContextBuilder, ContextWrapper, GlRequest, PossiblyCurrent,
 };
 
 #[cfg(target_os = "linux")]
@@ -62,95 +62,140 @@ impl Triangle {
 	}
 }
 
-// Much of this code is taken from the glow hello example, linked below
-// https://github.com/grovesNL/glow/tree/main/examples/hello
-// Comments are mostly for myself, but you might find them useful, too.
+struct GlWindow {
+	window: ContextWrapper<PossiblyCurrent, Window>,
+	gl: glow::Context,
+}
 
-pub fn run() {
-	let event_loop = EventLoop::new();
+impl GlWindow {
+	pub fn new<T>(event_loop: &EventLoopWindowTarget<T>, title: &str) -> Self {
+		let window = Self::create_window(event_loop, title);
 
-	#[cfg(target_os = "android")]
-	let window_builder = WindowBuilder::new().with_title("GlAndroid");
-
-	// A quality of life thing for myself. Give the window an App ID under wayland
-	// so that I can configure it to float instead of tiling with the rest of my
-	// windows.
-	// If you don't need this, you can remove the cfg's and just have the first
-	// window_builder.
-	#[cfg(target_os = "linux")]
-	let window_builder = WindowBuilder::new()
-		.with_title("GlAndroid")
-		.with_app_id("pleasefloat".into());
-
-	// Create a new window and make it the current context.
-	// unsafe is required here because `make_current` is unsafe. I believe this
-	// is the case because if it fails, we don't know what state we're left in
-	let window = unsafe {
-		ContextBuilder::new()
-			.with_vsync(true)
-			.build_windowed(window_builder, &event_loop)
-			.unwrap()
-			.make_current()
-			.unwrap()
-	};
-
-	// unsafe land! it would be good to abstract the unsafe away, but I'd like
-	// to keep this example simple. You can do it yourself, you're pretty smart!
-	let (gl, program, triangle) = unsafe {
 		// Get the OpenGL context.
 		// What is going on here? Why's it look so weird?
 		// `get_proc_address` apparently returns a pointer to an OpenGL function.
 		// It's put in a closure with a &str as an argument and passed to `from_loader_function`
 		// so that glow can call it with the name of every function it needs. OpenGL is weird and
 		// this is how extensions are loaded I think?
-		let gl = glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _);
+		let gl = unsafe {
+			glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _)
+		};
 
-		// Create a siny new shader program and then use it
-		let program = create_program(&gl);
-		gl.use_program(Some(program));
+		Self { window, gl }
+	}
 
-		// Create vertex buffer/array
-		let triangle = Triangle::new(&gl);
+	fn create_window<T>(
+		event_loop: &EventLoopWindowTarget<T>,
+		title: &str,
+	) -> ContextWrapper<PossiblyCurrent, Window> {
+		#[cfg(target_os = "android")]
+		let window_builder = WindowBuilder::new().with_title(title);
 
-		//gl.clear_color(0.1, 0.5, 0.3, 1.0);
-		gl.clear_color(0.0, 0.0, 0.0, 1.0);
+		// A quality of life thing for myself. Give the window an App ID under wayland
+		// so that I can configure it to float instead of tiling with the rest of my
+		// windows.
+		// If you don't need this, you can remove the cfg's and just have the first
+		// window_builder.
+		#[cfg(target_os = "linux")]
+		let window_builder = WindowBuilder::new()
+			.with_title(title)
+			.with_app_id("pleasefloat".into());
 
-		(gl, program, triangle)
-	};
+		// Create a new window and make it the current context.
+		// unsafe is required here because `make_current` is unsafe. I believe this
+		// is the case because if it fails, we don't know what state we're left in
+		unsafe {
+			ContextBuilder::new()
+				.with_gl(GlRequest::Specific(glutin::Api::OpenGlEs, (3, 0)))
+				.with_vsync(true)
+				.build_windowed(window_builder, &event_loop)
+				.unwrap()
+				.make_current()
+				.unwrap()
+		}
+	}
+}
 
-	// Main window event loop. We'll stay here for the life of the program
-	event_loop.run(move |event, _, control_flow| {
-		*control_flow = ControlFlow::Wait; // suspend the thread until new events arrive
+struct GlObjects {
+	program: Program,
+	triangle: Triangle,
+}
 
+struct App {
+	title: String,
+	running: bool,
+	paused: bool,
+	glwindow: Option<GlWindow>,
+	objects: Option<GlObjects>,
+}
+
+impl App {
+	pub fn new(title: String) -> Self {
+		Self {
+			title,
+			running: false,
+			paused: false,
+			glwindow: None,
+			objects: None,
+		}
+	}
+
+	pub fn run(&mut self) {
+		dbg!("Running!");
+		self.running = true;
+
+		let mut el = EventLoop::new();
+
+		#[cfg(not(target_os = "android"))]
+		{
+			self.create_window(&el);
+			self.init_opengl();
+		}
+
+		while self.running {
+			el.run_return(|event, event_loop, control_flow| {
+				*control_flow = ControlFlow::Wait;
+
+				self.process_events(event, event_loop, control_flow);
+			});
+		}
+	}
+
+	fn process_events(
+		&mut self,
+		event: Event<()>,
+		event_loop: &EventLoopWindowTarget<()>,
+		control_flow: &mut ControlFlow,
+	) {
 		match event {
-			Event::RedrawRequested(_window_id) => unsafe {
-				// First we clear!
-				gl.clear(glow::COLOR_BUFFER_BIT); // http://docs.gl/es3/glClear
+			Event::Resumed => {
+				dbg!("received Event::Resumed");
+				self.paused = false;
 
-				// Then we draw!
-				triangle.bind(&gl);
-				gl.draw_arrays(glow::TRIANGLES, 0, 3);
-
-				// Swap the buffer we just drew to with the one that's currently displayed
-				window.swap_buffers().unwrap();
-			},
-			Event::WindowEvent { event, .. } => match event {
-				WindowEvent::Resized(physical_size) => {
-					window.resize(physical_size);
-					unsafe {
-						gl.viewport(
-							0,
-							0,
-							physical_size.width as i32,
-							physical_size.height as i32,
-						)
-					};
+				#[cfg(target_os = "android")]
+				if ndk_glue::native_window().is_some() {
+					self.create_window(event_loop);
+					self.init_opengl();
 				}
+			}
+			Event::Suspended => {
+				dbg!("received Event::Suspended");
+				self.paused = true;
+
+				#[cfg(target_os = "android")]
+				self.destroy_window();
+			}
+			Event::RedrawRequested(_window_id) => self.draw(),
+			Event::WindowEvent { event, .. } => match event {
+				WindowEvent::Resized(physical_size) => self.resize(physical_size),
 				WindowEvent::CloseRequested => {
-					// Cleanup OpenGL things
-					unsafe {
-						gl.delete_program(program);
-						triangle.delete(&gl);
+					if let Some(ref glw) = self.glwindow {
+						let objects = self.objects.as_ref().unwrap();
+						// Cleanup OpenGL things
+						unsafe {
+							glw.gl.delete_program(objects.program);
+							objects.triangle.delete(&glw.gl);
+						}
 					}
 					// Finally, Exit!
 					*control_flow = ControlFlow::Exit;
@@ -159,42 +204,114 @@ pub fn run() {
 			},
 			_ => (),
 		}
-	});
-}
+	}
 
-unsafe fn create_program(gl: &glow::Context) -> Program {
-	// In OpenGL, shaders are bound to a program
-	let program = gl.create_program().expect("Failed to create program");
-
-	let shader_soruces = [
-		(glow::VERTEX_SHADER, include_str!("shaders/tri.vert")),
-		(glow::FRAGMENT_SHADER, include_str!("shaders/tri.frag")),
-	];
-
-	let mut shaders = vec![];
-	for (stype, source) in shader_soruces.iter() {
-		let shader = gl.create_shader(*stype).expect("Failed to create shader");
-		gl.shader_source(shader, source);
-		gl.compile_shader(shader);
-
-		if !gl.get_shader_compile_status(shader) {
-			panic!("{}", gl.get_shader_info_log(shader));
+	fn draw(&self) {
+		if self.paused {
+			return;
 		}
 
-		gl.attach_shader(program, shader);
-		shaders.push(shader);
+		if let Some(ref window) = self.glwindow {
+			let objects = self.objects.as_ref().unwrap();
+			unsafe {
+				// First we clear!
+				window.gl.clear(glow::COLOR_BUFFER_BIT); // http://docs.gl/es3/glClear
+
+				// Then we draw!
+				objects.triangle.bind(&window.gl);
+				window.gl.draw_arrays(glow::TRIANGLES, 0, 3);
+
+				// Swap the buffer we just drew to with the one that's currently displayed
+				window.window.swap_buffers().unwrap();
+			}
+		}
 	}
 
-	gl.link_program(program);
-	if !gl.get_program_link_status(program) {
-		panic!("{}", gl.get_program_info_log(program));
+	fn resize(&self, physical_size: PhysicalSize<u32>) {
+		if let Some(ref window) = self.glwindow {
+			window.window.resize(physical_size);
+			unsafe {
+				window.gl.viewport(
+					0,
+					0,
+					physical_size.width as i32,
+					physical_size.height as i32,
+				)
+			};
+		}
 	}
 
-	// Shaders are compiled and linked with the program, we don't need them anymore
-	for shader in shaders {
-		gl.detach_shader(program, shader);
-		gl.delete_shader(shader);
+	fn create_window(&mut self, event_loop: &EventLoopWindowTarget<()>) {
+		self.glwindow = Some(GlWindow::new(event_loop, &self.title));
 	}
 
-	program
+	fn destroy_window(&mut self) {
+		self.glwindow = None;
+	}
+
+	// Generate all the OpenGL resources
+	fn init_opengl(&mut self) {
+		if let Some(ref window) = self.glwindow {
+			let gl = &window.gl;
+
+			unsafe {
+				gl.clear_color(0.1, 0.5, 0.3, 1.0);
+
+				// Create a shiny new shader program and then use it
+				let program = Self::create_program(&gl);
+				gl.use_program(Some(program));
+
+				// Create vertex buffer/array
+				let triangle = Triangle::new(&gl);
+
+				self.objects = Some(GlObjects { program, triangle });
+			}
+		}
+	}
+
+	unsafe fn create_program(gl: &glow::Context) -> Program {
+		// In OpenGL, shaders are bound to a program
+		let program = gl.create_program().expect("Failed to create program");
+
+		let shader_soruces = [
+			(glow::VERTEX_SHADER, include_str!("shaders/tri.vert")),
+			(glow::FRAGMENT_SHADER, include_str!("shaders/tri.frag")),
+		];
+
+		let mut shaders = vec![];
+		for (stype, source) in shader_soruces.iter() {
+			let shader = gl.create_shader(*stype).expect("Failed to create shader");
+			gl.shader_source(shader, source);
+			gl.compile_shader(shader);
+
+			if !gl.get_shader_compile_status(shader) {
+				panic!("{}", gl.get_shader_info_log(shader));
+			}
+
+			gl.attach_shader(program, shader);
+			shaders.push(shader);
+		}
+
+		gl.link_program(program);
+		if !gl.get_program_link_status(program) {
+			panic!("{}", gl.get_program_info_log(program));
+		}
+
+		// Shaders are compiled and linked with the program, we don't need them anymore
+		for shader in shaders {
+			gl.detach_shader(program, shader);
+			gl.delete_shader(shader);
+		}
+
+		program
+	}
+}
+
+// Much of this code is taken from the glow hello example, linked below
+// https://github.com/grovesNL/glow/tree/main/examples/hello
+// Comments are mostly for myself, but you might find them useful, too.
+
+pub fn run() {
+	let mut app = App::new("GlAndroid".into());
+	app.run();
 }
